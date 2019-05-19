704910bcb032750d7970d6bd0e3e0ae0cd81f80f
hirotosuzuki <hiroto7665@gmail.com>
2019-05-19 12:47:12 +0900

    [add]selection sort

---
+++ alds_1_2_b.py
@@ -0,0 +1,24 @@
+import time
+
+# Selection Sort
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    counter = 0
+    for i in range(0, N):
+        minj = i
+        for j in range(i, N):
+            if A[j]<A[minj]:
+                minj = j
+        if i!=minj:
+            counter += 1
+            A[i], A[minj] = A[minj], A[i]   
+    print(' '.join(map(str, A)))
+    print(counter)
+
+
+if __name__=='__main__':
+    start = time.time()
+    main()
+    elapsed_time = time.time() - start
+    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
