31ab84f708fe684bed2d256719964bbf98fd7178
hirotosuzuki <hiroto7665@gmail.com>
2019-06-04 23:55:42 +0900

    [add]1_4_a~1_4_d

---
+++ alds_1_4_a.py
@@ -0,0 +1,9 @@
+
+def main():
+    n = int(input())
+    S = [int(i) for i in input().split()]
+    q = int(input())
+    T = [int(i) for i in input().split()]
+    print(len(set(S) & set(T)))
+
+main()
---
+++ alds_1_4_c.py
@@ -0,0 +1,17 @@
+class HashDict:
+    def __init__(self):
+        pass
+    def insert(self, string):
+        pass
+    def find(self, string):
+        pass
+
+
+def main():
+    n = int(input())
+    for i in range(n):
+        order, moji = input().split()
+        print(moji)
+
+if __name__=='__main__':
+    main()
